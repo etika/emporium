59fa243953601c1dba8903dec323294a8fb3806c
etika <etikaahuja@gmail.com>
2016-05-10 20:04:28 +0530

    Changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
 
   def remove_book_link(text,book)
     link_to text,{:url=>{:controller=>"cart",:action=>"remove",:id=>book}},{:title=>"Remove Cart",:href=>url_for(:controller=>"cart",:action=>"remove",:id=>book)},:remote=>true
-
+  end
   def clear_cart_link(text = "Clear Cart")
     link_to text,{:url=>{:controller=>"cart",:action=>"clear"}},{:href=>url_for(:controller=>"cart",:action=>"clear",:id=>book)},:remote=>true
   end
